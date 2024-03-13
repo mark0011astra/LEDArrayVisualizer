ad32ccbbe666b0bd3ef9aae467b136c4bb0b3eff
Mark <mark0011astra@gmail.com>
2024-03-13 18:55:09 +0900

    LEDArrayVisualizerのクリアメソッドを修正し、明るさを0に設定するように変更しました。

--- LEDArrayVisualizer.h
+++ LEDArrayVisualizer.h
@@ -5,35 +5,36 @@
 
 class LEDArrayVisualizer {
 public:
-    LEDArrayVisualizer(uint8_t dataPin, uint8_t clkPin, uint8_t csPin, uint8_t numDevices);
-    void begin();
-    void displayArray(int arr[8][8]);
-    void clear();
+  LEDArrayVisualizer(uint8_t dataPin, uint8_t clkPin, uint8_t csPin, uint8_t numDevices);
+  void begin();
+  void displayArray(int arr[8][8]);
+  void clear();
 
 private:
-    MD_MAX72XX mx;
+  MD_MAX72XX mx;
 };
 
 LEDArrayVisualizer::LEDArrayVisualizer(uint8_t dataPin, uint8_t clkPin, uint8_t csPin, uint8_t numDevices)
-    : mx(MD_MAX72XX::FC16_HW, dataPin, clkPin, csPin, numDevices) {}
+  : mx(MD_MAX72XX::FC16_HW, dataPin, clkPin, csPin, numDevices) {}
 
 void LEDArrayVisualizer::begin() {
-    mx.begin();
+  mx.begin();
+  mx.control(MD_MAX72XX::INTENSITY, 0);
+  mx.clear();
 }
 
 void LEDArrayVisualizer::displayArray(int arr[8][8]) {
-    clear();
-    for (uint8_t row = 0; row < 8; row++) {
-        for (uint8_t col = 0; col < 8; col++) {
-        mx.setPoint(row, col, arr[row][col] != 0);
-        }
+  for (uint8_t row = 0; row < 8; row++) {
+    for (uint8_t col = 0; col < 8; col++) {
+      mx.setPoint(row, col, arr[row][col] != 0);
     }
-    mx.update();
+  }
+  mx.update();
 }
 
 void LEDArrayVisualizer::clear() {
-    mx.clear();
-    mx.update();
+  mx.clear();
+  mx.update();
 }
 
 #endif
